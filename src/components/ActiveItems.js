ddd472fda037b53c18344cf0c70ff7f8da8f6f43
Kristoffer Nordkvist <kristoffer.nordkvist@gmail.com>
2020-07-05 00:10:35 +0200

    Extract AddItem function into AddItem.js

--- src/components/ActiveItems.js
+++ src/components/ActiveItems.js
@@ -1,37 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import ItemList from './ItemList';
 import { AppStateContext } from '../context/app-state';
-
-function AddItem() {
-  const { addItem } = useContext(AppStateContext);
-  const initialInputText = '';
-  const [text, setText] = useState(initialInputText);
-
-  const addItemAndClearInput = (text) => {
-    setText(initialInputText);
-    addItem(text);
-  };
-
-  return (
-    <li className="item-add">
-      <label>
-        <span aria-label="Plus sign" role="img">
-          ➕
-        </span>
-        <input
-          type="text"
-          placeholder="todo..."
-          value={text}
-          onChange={(event) => setText(event.target.value)}
-          onKeyDown={(event) =>
-            event.key === 'Enter' && addItemAndClearInput(text)
-          }
-          autoFocus
-        />
-      </label>
-    </li>
-  );
-}
+import { AddItem } from './AddItem';
 
 export default function ActiveItems() {
   const { state } = useContext(AppStateContext);
---
+++ src/components/AddItem.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react';
+import { AppStateContext } from '../context/app-state';
+
+export function AddItem() {
+  const { addItem } = useContext(AppStateContext);
+  const initialInputText = '';
+  const [text, setText] = useState(initialInputText);
+
+  const addItemAndClearInput = (text) => {
+    setText(initialInputText);
+    addItem(text);
+  };
+
+  return (
+    <li className="item-add">
+      <label>
+        <span aria-label="Plus sign" role="img">
+          ➕
+        </span>
+        <input
+          type="text"
+          placeholder="todo..."
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          onKeyDown={(event) =>
+            event.key === 'Enter' && addItemAndClearInput(text)
+          }
+          autoFocus
+        />
+      </label>
+    </li>
+  );
+}
