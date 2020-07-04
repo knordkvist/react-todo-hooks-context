1a952883e551e058019b06f294dd9cda8ae4f151
Kristoffer Nordkvist <kristoffer.nordkvist@gmail.com>
2020-07-05 00:57:03 +0200

    Move component Item into new file

---
+++ src/components/Item.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { Item as ReducerItem } from '../context/Item';
+import { AppStateContext } from '../context/app-state';
+const ItemState = ReducerItem.State;
+
+export default function Item({ item }) {
+  const { completeItem, uncheckItem, editItem } = useContext(AppStateContext);
+
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={item.state === ItemState.Completed}
+          data-testid={item.id}
+          onChange={() =>
+            item.state === ItemState.Completed
+              ? uncheckItem(item.id)
+              : completeItem(item.id)
+          }
+        />
+        <input
+          className="item-text-input"
+          type="text"
+          value={item.text}
+          data-item-id={item.id}
+          onChange={(e) => editItem(item.id, e.target.value)}
+        />
+      </label>
+    </li>
+  );
+}
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,35 +1,5 @@
-import React, { useContext } from 'react';
-import { Item as ReducerItem } from '../context/Item';
-import { AppStateContext } from '../context/app-state';
-const ItemState = ReducerItem.State;
-
-function Item({ item }) {
-  const { completeItem, uncheckItem, editItem } = useContext(AppStateContext);
-
-  return (
-    <li>
-      <label>
-        <input
-          type="checkbox"
-          checked={item.state === ItemState.Completed}
-          data-testid={item.id}
-          onChange={() =>
-            item.state === ItemState.Completed
-              ? uncheckItem(item.id)
-              : completeItem(item.id)
-          }
-        />
-        <input
-          className="item-text-input"
-          type="text"
-          value={item.text}
-          data-item-id={item.id}
-          onChange={(e) => editItem(item.id, e.target.value)}
-        />
-      </label>
-    </li>
-  );
-}
+import React from 'react';
+import Item from './Item';
 
 export default function ItemList({ items, children }) {
   return (
