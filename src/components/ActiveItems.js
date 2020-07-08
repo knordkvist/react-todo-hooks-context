03876e777cc66636189073674bc3b980f0b6fedd
Kristoffer Nordkvist <kristoffer.nordkvist@gmail.com>
2020-07-08 15:09:13 +0200

    Simplify ActiveItems component by moving all focus responsibility into the Focus component

--- src/components/ActiveItems.js
+++ src/components/ActiveItems.js
@@ -1,49 +1,34 @@
-import React, {
-  useContext,
-  forwardRef,
-  useImperativeHandle,
-  useRef,
-  useEffect,
-} from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import ItemList from './ItemList';
 import { AppStateContext } from '../context/app-state';
 import { AddItem } from './AddItem';
 
-const Focusable = forwardRef(({ items, render }, ref) => {
+function Focusable({ items, render }) {
+  const { state } = useContext(AppStateContext);
   const itemRefs = useRef({});
   const setItemRef = (itemId) => (itemRef) =>
     (itemRefs.current[itemId] = itemRef);
 
-  useImperativeHandle(ref, () => ({
-    focusTextInput: (itemId) => {
-      itemRefs.current[itemId].focusTextInput();
-    },
-  }));
+  // When a new item is added, focus that specific input
+  useEffect(() => {
+    if (state.activeItems.length === 0) return;
+
+    const { id: itemId } = state.activeItems[state.activeItems.length - 1];
+    itemRefs.current[itemId].focusTextInput(itemId);
+  }, [state.activeItems]);
 
   return items.map((item) => {
     // Pass a callback ref to the item
     return render(item, { ref: setItemRef(item.id) });
   });
-});
+}
 
 export default function ActiveItems() {
   const { state } = useContext(AppStateContext);
-  const wrapperRef = useRef();
-
-  // When a new item is added, tell the wrapper to focus that specific input
-  useEffect(() => {
-    if (state.activeItems.length === 0) return;
-
-    const latestItem = state.activeItems[state.activeItems.length - 1];
-    wrapperRef.current.focusTextInput(latestItem.id);
-  }, [state.activeItems]);
 
   return (
     <div data-testid="active-items-container">
-      <ItemList
-        items={state.activeItems}
-        ItemWrapper={(props) => <Focusable {...props} ref={wrapperRef} />}
-      >
+      <ItemList items={state.activeItems} ItemWrapper={Focusable}>
         <AddItem />
       </ItemList>
     </div>
