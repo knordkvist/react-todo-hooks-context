d04ea7c4643ff4eb7a410a0cad249cf7c5412d3e
Kristoffer Nordkvist <kristoffer.nordkvist@gmail.com>
2020-07-11 23:49:52 +0200

    Simplify reducer by replacing set and find functions with a default item

--- src/context/app-reducer.js
+++ src/context/app-reducer.js
@@ -10,36 +10,29 @@ import {
   splitItem,
 } from './reducer-actions';
 
-export default produce((draft, { type, payload } = {}) => {
-  const set = (setter) => {
-    const item = find();
-    setter(item);
-  };
-
-  const find = (itemId = payload.id) =>
-    draft.items.find((item) => item.id === itemId);
+export default produce((draft, { type, payload = {} } = {}) => {
+  const item = draft.items.find((item) => item.id === payload.id);
 
   switch (type) {
     case addItem.type:
       draft.items.push(new Item(payload));
       return;
     case completeItem.type:
-      set((item) => (item.state = Item.State.Completed));
+      item.state = Item.State.Completed;
       return;
     case uncheckItem.type: {
-      set((item) => (item.state = Item.State.Active));
+      item.state = Item.State.Active;
       return;
     }
     case editItem.type: {
-      set((item) => (item.text = payload.text));
+      item.text = payload.text;
       return;
     }
     case splitItem.type: {
-      const item = find();
       const textLeft = item.text.slice(0, payload.splitAt);
       const textRight = item.text.slice(payload.splitAt);
       const itemIndex = draft.items.indexOf(item);
-      set((item) => (item.text = textLeft));
+      item.text = textLeft;
       draft.items.splice(
         itemIndex + 1,
         0,
