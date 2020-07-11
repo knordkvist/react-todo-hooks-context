4b3292d5cb7a2ad676913362c16dad5b73ebcad9
Kristoffer Nordkvist <kristoffer.nordkvist@gmail.com>
2020-07-11 22:08:01 +0200

    Move styles from index.css into AddItem

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,11 +1,26 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { AppStateContext } from '../context/app-state';
+
+const ListItem = styled.li`
+  .item-add-img {
+    background: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjE4cHgiIHdpZHRoPSIxOHB4IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0OCA0OCIgZmlsbD0iIzAwMDAwMCI+CiA8cGF0aCBkPSJtMzggMjZoLTEydjEyaC00di0xMmgtMTJ2LTRoMTJ2LTEyaDR2MTJoMTJ2NHoiLz4KIDxwYXRoIGQ9Im0wIDBoNDh2NDhoLTQ4eiIgZmlsbD0ibm9uZSIvPgo8L3N2Zz4K)
+      no-repeat center;
+    width: 13px;
+    height: 13px;
+    margin-bottom: 3px;
+    margin-left: 4px;
+    margin-right: 3px;
+    margin-top: 3px;
+    opacity: 0.54;
+  }
+`;
 
 export function AddItem() {
   const { addItem } = useContext(AppStateContext);
 
   return (
-    <li className="item-add">
+    <ListItem>
       <label>
         <span className="item-add-img" aria-label="Plus sign" role="img"></span>
         <input
@@ -16,6 +31,6 @@ export function AddItem() {
           autoFocus
         />
       </label>
-    </li>
+    </ListItem>
   );
 }
