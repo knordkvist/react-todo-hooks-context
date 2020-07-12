d84be07d7e8f27acfc857510a7e0b4c5568b611c
Kristoffer Nordkvist <kristoffer.nordkvist@gmail.com>
2020-07-12 18:59:48 +0200

    Clean up AppStateProvider

--- src/context/app-state.js
+++ src/context/app-state.js
@@ -1,14 +1,7 @@
 import React, { createContext, useReducer, useState } from 'react';
 
 import appReducer from './app-reducer';
-import {
-  addItem,
-  completeItem,
-  uncheckItem,
-  editItem,
-  splitItem,
-  mergeItem,
-} from './reducer-actions';
+import * as actions from './reducer-actions';
 
 export const AppStateContext = createContext();
 
@@ -24,47 +17,50 @@ export const AppStateProvider = ({ children }) => {
   const sendEvent = (eventType, data) =>
     setLatestEvent({ type: eventType, data });
   const [instructionsVisible, setInstructionsVisible] = useState(true);
-  const dismissInstructions = () => setInstructionsVisible(false);
 
-  const dispatchCompleteItem = (itemId) => dispatch(completeItem(itemId));
-  const dispatchUncheckItem = (itemId) => dispatch(uncheckItem(itemId));
-  const dispatchEditItem = (itemId, text) => dispatch(editItem(itemId, text));
+  const dismissInstructions = () => setInstructionsVisible(false);
+  const completeItem = (itemId) => dispatch(actions.completeItem(itemId));
+  const uncheckItem = (itemId) => dispatch(actions.uncheckItem(itemId));
+  const editItem = (itemId, text) => dispatch(actions.editItem(itemId, text));
+  const addItem = (text) => {
+    const action = actions.addItem({ text });
+    dispatch(action);
+    sendEvent(EventType.ItemAdded, {
+      itemId: action.payload.id,
+      text,
+    });
+    dismissInstructions();
+  };
+  const splitItem = (itemId, splitAt) => {
+    const action = actions.splitItem(itemId, splitAt);
+    dispatch(action);
+    sendEvent(EventType.ItemSplit, {
+      splitItemId: itemId,
+      newItemId: action.payload.newItemId,
+    });
+  };
+  const mergeItem = (itemId) => {
+    const removedItemIndex = todoItems.items.findIndex(
+      (item) => item.id === itemId
+    );
+
+    if (removedItemIndex === 0) return;
+
+    dispatch(actions.mergeItem(itemId));
+    const mergedIntoItem = todoItems.items[removedItemIndex - 1];
+    sendEvent(EventType.ItemMerged, { mergedIntoItem });
+  };
 
   return (
     <AppStateContext.Provider
       value={{
         todoItems,
-        addItem: (text) => {
-          const action = addItem({ text });
-          dispatch(action);
-          sendEvent(EventType.ItemAdded, {
-            itemId: action.payload.id,
-            text,
-          });
-          dismissInstructions();
-        },
-        completeItem: dispatchCompleteItem,
-        uncheckItem: dispatchUncheckItem,
-        editItem: dispatchEditItem,
-        splitItem: (itemId, splitAt) => {
-          const action = splitItem(itemId, splitAt);
-          dispatch(action);
-          sendEvent(EventType.ItemSplit, {
-            splitItemId: itemId,
-            newItemId: action.payload.newItemId,
-          });
-        },
-        mergeItem: (itemId) => {
-          const removedItemIndex = todoItems.items.findIndex(
-            (item) => item.id === itemId
-          );
-
-          if (removedItemIndex === 0) return;
-
-          dispatch(mergeItem(itemId));
-          const mergedIntoItem = todoItems.items[removedItemIndex - 1];
-          sendEvent(EventType.ItemMerged, { mergedIntoItem });
-        },
+        addItem,
+        completeItem,
+        uncheckItem,
+        editItem,
+        splitItem,
+        mergeItem,
         instructionsVisible,
         dismissInstructions,
         latestEvent,
