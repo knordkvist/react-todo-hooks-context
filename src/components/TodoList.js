e6e00f27da786a6fec5d269a96eca5c84e418377
Kristoffer Nordkvist <kristoffer.nordkvist@gmail.com>
2020-07-17 15:58:00 +0200

    Merge AppContainer into TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import AppContainer from './AppContainer';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { AppStateContext } from '../context/app-state';
 import Header from './Header';
 import ActiveItems from './ActiveItems';
 import CompletedItems from './CompletedItems';
 import Instructions from './Instructions';
 
+const AppContainer = styled.div`
+  position: relative;
+  box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+    0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
+  padding: 6px 18px;
+  border-radius: 4px;
+  min-width: 300px;
+  width: 100%;
+  max-width: 560px;
+  margin-top: 30px;
+  padding-bottom: ${(props) =>
+    props.instructionsVisible ? props.theme.instructionsHeight : false};
+  background-color: ${(props) => props.theme.backgroundColorPrimary};
+  * {
+    background-color: ${(props) => props.theme.backgroundColorPrimary};
+  }
+`;
+
 export default function TodoList() {
+  const { instructionsVisible } = useContext(AppStateContext);
+
   return (
-    <AppContainer>
+    <AppContainer instructionsVisible={instructionsVisible}>
       <Header />
       <ActiveItems />
       <CompletedItems />
